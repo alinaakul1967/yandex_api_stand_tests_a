a0cf12fe94d0c3ee7893b48774eee5716b90d6b8
alinaakul <alinaakul@yandex.ru>
2023-10-25 11:45:22 +0300

    My first commit

---
+++ data.py
@@ -0,0 +1,13 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Анатолий",
+    "phone": "+74441237887",
+    "address": "г. Москва, ул. Хохотушкина, д. 16"
+}
+
+product_ids = {
+    "ids": [1, 2, 3]
+}
